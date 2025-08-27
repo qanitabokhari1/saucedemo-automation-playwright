c85a471b735658856abf2736ca9914c1bbde23d4
Qanita Syed <33183837+qanitabokhari1@users.noreply.github.com>
2025-08-27 16:43:07 +0500

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    '@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/await-thenable': 'error',
+    '@typescript-eslint/no-misused-promises': 'error',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'prefer-template': 'error',
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  ignorePatterns: [
+    'dist/',
+    'node_modules/',
+    'test-results/',
+    '*.js'
+  ],
+};
